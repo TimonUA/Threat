ab9ab60245a63590124dd364fbbae7824cad6845
TimonUA <57059072+TimonUA@users.noreply.github.com>
2021-02-14 23:11:29 +0200

    1

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject characterPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<6;i++)
+        {
+            Instantiate(characterPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
