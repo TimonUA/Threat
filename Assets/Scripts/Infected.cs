b8dbed7321063600d23668e19b14cf9b2f0b5b5d
TimonUA <57059072+TimonUA@users.noreply.github.com>
2021-03-08 11:41:11 +0200

    1

--- Assets/Scripts/Infected.cs
+++ Assets/Scripts/Infected.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Infected : MonoBehaviour
 {
     private bool IsCollision;
+    private float ChanceToInfect;
+    private int rand;
+    private List<GameObject> UnderThreat=new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +15,9 @@ public class Infected : MonoBehaviour
         gameObject.tag = "Infected";
         gameObject.AddComponent<Rigidbody2D>();
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-        gameObject.GetComponent<BoxCollider2D>().edgeRadius = 1.2f;
-        gameObject.GetComponent<BoxCollider2D>().size = new Vector2(3f, 1f);
+        gameObject.GetComponent<CircleCollider2D>().radius = 2.6f;
         InvokeRepeating("Test", 0f, 2f);
+        InvokeRepeating("Disease", 0f, 2f);      
     }
 
     // Update is called once per frame
@@ -25,19 +28,54 @@ public class Infected : MonoBehaviour
     {
         if (collider.tag == "Character")
         {         
-            IsCollision = true;              
+            IsCollision = true;
+            UnderThreat.Add(collider.gameObject);
         }
     }
     void Test()
     {
-        if(IsCollision)
-        Debug.Log("Trigger");
+        if (IsCollision)
+        {
+            ChanceToInfect = Random.value;
+            for (int i=0;i<UnderThreat.Count;i++)
+            {
+                if (ChanceToInfect < 0.2)
+                {
+                    rand = Random.Range(0, UnderThreat.Count);
+                    UnderThreat[i].GetComponent<Character>().IsInfected = true;
+                    UnderThreat[i].AddComponent<Infected>();
+                    if (UnderThreat.Count > 1)
+                    {
+                        UnderThreat.Remove(UnderThreat[i]);
+                    }
+                    else
+                    {
+                        IsCollision = false;
+                        UnderThreat.Remove(UnderThreat[i]);
+                    }
+
+                }
+            }         
+            Debug.Log(UnderThreat.Count);
+        }
+    }
+    void Disease()
+    {
+
     }
     private void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.tag == "Character")
         {            
-            IsCollision = false;
+            if(UnderThreat.Count>1)
+            {             
+                UnderThreat.Remove(collider.gameObject);
+            }
+            else
+            {
+                IsCollision = false;
+                UnderThreat.Remove(collider.gameObject);
+            }
         }
     }
 }
